10af22f2a07acfb1763a73fb5be3b2b9ee06713c
Gabriele <gabboxl0@gmail.com>
2019-05-20 17:57:58 +0200

    Initial release of the script

---
+++ nowplaying.php
@@ -0,0 +1,39 @@
+<?php
+
+function nowPlaying($returnvariable = null)
+{
+    $url = 'https://icstream.rds.radio/status-json.xsl';  //vekkio http://stream1.rds.it:8000/status-json.xsl
+    $jsonroba = file_get_contents($url);
+    $jsonclear = json_decode($jsonroba, true);
+    $metadata = explode('*', $jsonclear['icestats']['source'][15]['title']);
+
+    //anti-floodwait
+    file_put_contents('testmoseca.php', $jsonclear['icestats']['source'][15]['title']);
+    if ($returnvariable == 'jsonclear') {
+        return $jsonclear['icestats']['source'][15]['title'];
+    }
+
+    return $metadata;
+}
+
+function clearLine()
+{
+    echo "\033[2K\r";
+}
+
+$icsd = 5;
+$miao = nowPlaying("jsonclear");
+while(1){
+  if($icsd == 0){
+    echo "\033[5D";
+    $miao = nowPlaying("jsonclear");
+    echo($miao."    (aggiornamento...)"."\r");
+    $icsd = 5;
+    sleep(1);
+  }else{
+    clearLine();
+    echo($miao."    ($icsd)"."\r");
+    $icsd--;
+    sleep(1);
+  }
+}
